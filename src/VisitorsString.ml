ceb22cf7bc5d9ae4a329a4e223eb5243f3abe313
François Pottier <francois.pottier@inria.fr>
2016-12-23 16:23:30 +0100

    Auxiliary functions [prefix] and [remainder].

--- src/VisitorsString.ml
+++ src/VisitorsString.ml
@@ -1,5 +1,21 @@
 open String
 
+(* [prefix s1 s2] tests whether [s1] is a prefix of [s2], i.e. whether
+   [s2] begins with [s1]. *)
+
+let prefix s1 s2 =
+  let n1 = length s1 in
+  n1 <= length s2 && s1 = sub s2 0 n1
+
+(* [remainder s1 s2] assumes that [s1] is a prefix of [s2], and chops
+   [s1] off [s2], returning the remainder. *)
+
+let remainder s1 s2 =
+  assert (prefix s1 s2);
+  let n1 = length s1 in
+  let n2 = length s2 in
+  sub s2 n1 (n2 - n1)
+
 (* [split_on_char c s] splits the string [s] into substrings delimited
    by the character [c]. *)
 
