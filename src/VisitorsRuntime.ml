129cad865c9410eb1da6d9a7074341da5f1eaaff
François Pottier <francois.pottier@inria.fr>
2016-12-23 09:20:20 +0100

    More code in VisitorsRuntime.

--- src/VisitorsRuntime.ml
+++ src/VisitorsRuntime.ml
@@ -3,7 +3,48 @@ exception StructuralMismatch
 let fail () =
   raise StructuralMismatch
 
+module Array = struct
+
+  let iter f env xs =
+    Array.iter (f env) xs
+
+  let map f env xs =
+    Array.map (f env) xs
+
+  let iter2 f env xs1 xs2 =
+    if Array.length xs1 = Array.length xs2 then
+      Array.iter2 (f env) xs1 xs2
+    else
+      fail()
+
+  let map2 f env xs1 xs2 =
+    if Array.length xs1 = Array.length xs2 then
+      Array.map2 (f env) xs1 xs2
+    else
+      fail()
+
+end
+
 module List = struct
+
+  (* We could reuse the functions provided by OCaml's [List] module,
+     as we did above for arrays. *)
+
+  let rec iter f env xs =
+    match xs with
+    | [] ->
+        ()
+    | x :: xs ->
+        f env x;
+        iter f env xs
+
+  let rec map f env xs =
+    match xs with
+    | [] ->
+        []
+    | x :: xs ->
+        let x = f env x in
+        x :: map f env xs
 
   let rec iter2 f env xs1 xs2 =
     match xs1, xs2 with
@@ -29,7 +70,21 @@ end
 
 module Option = struct
 
-  let rec iter2 f env ox1 ox2 =
+  let iter f env ox =
+    match ox with
+    | None ->
+        ()
+    | Some x ->
+        f env x
+
+  let map f env ox =
+    match ox with
+    | None ->
+        None
+    | Some x ->
+        Some (f env x)
+
+  let iter2 f env ox1 ox2 =
     match ox1, ox2 with
     | None, None ->
         ()
@@ -38,7 +93,7 @@ module Option = struct
     | _, _ ->
         fail()
 
-  let rec map2 f env ox1 ox2 =
+  let map2 f env ox1 ox2 =
     match ox1, ox2 with
     | None, None ->
         None
@@ -47,5 +102,21 @@ module Option = struct
         Some x
     | _, _ ->
         fail()
+
+end
+
+module Ref = struct
+
+  let iter f env rx =
+    f env !rx
+
+  let map f env rx =
+    ref (f env !rx)
+
+  let iter2 f env rx1 rx2 =
+    f env !rx1 !rx2
+
+  let map2 f env rx1 rx2 =
+    ref (f env !rx1 !rx2)
 
 end
--- src/VisitorsRuntime.mli
+++ src/VisitorsRuntime.mli
@@ -1,11 +1,29 @@
 exception StructuralMismatch
 
+module Array : sig
+  val iter: ('env -> 'a -> unit) -> 'env -> 'a array ->     unit
+  val map:  ('env -> 'a ->   'b) -> 'env -> 'a array -> 'b array
+  val iter2: ('env -> 'a -> 'b -> unit) -> 'env -> 'a array -> 'b array ->     unit
+  val map2:  ('env -> 'a -> 'b ->   'c) -> 'env -> 'a array -> 'b array -> 'c array
+end
+
 module List : sig
+  val iter: ('env -> 'a -> unit) -> 'env -> 'a list ->    unit
+  val map:  ('env -> 'a ->   'b) -> 'env -> 'a list -> 'b list
   val iter2: ('env -> 'a -> 'b -> unit) -> 'env -> 'a list -> 'b list ->    unit
   val map2:  ('env -> 'a -> 'b ->   'c) -> 'env -> 'a list -> 'b list -> 'c list
 end
 
 module Option : sig
+  val iter: ('env -> 'a -> unit) -> 'env -> 'a option ->      unit
+  val map:  ('env -> 'a ->   'b) -> 'env -> 'a option -> 'b option
   val iter2: ('env -> 'a -> 'b -> unit) -> 'env -> 'a option -> 'b option ->      unit
   val map2:  ('env -> 'a -> 'b ->   'c) -> 'env -> 'a option -> 'b option -> 'c option
+end
+
+module Ref : sig
+  val iter: ('env -> 'a -> unit) -> 'env -> 'a ref ->   unit
+  val map:  ('env -> 'a ->   'b) -> 'env -> 'a ref -> 'b ref
+  val iter2: ('env -> 'a -> 'b -> unit) -> 'env -> 'a ref -> 'b ref ->   unit
+  val map2:  ('env -> 'a -> 'b ->   'c) -> 'env -> 'a ref -> 'b ref -> 'c ref
 end
