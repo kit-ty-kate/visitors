bf68588613b761acef6d9bb4195c52150ec791d8
François Pottier <francois.pottier@inria.fr>
2016-12-24 14:09:03 +0100

    Remove [split_on_char].

--- src/VisitorsString.ml
+++ src/VisitorsString.ml
@@ -15,19 +15,3 @@ let remainder s1 s2 =
   let n1 = length s1 in
   let n2 = length s2 in
   sub s2 n1 (n2 - n1)
-
-(* [split_on_char c s] splits the string [s] into substrings delimited
-   by the character [c]. *)
-
-(* Copied from OCaml 4.04. *)
-
-let split_on_char sep s =
-  let r = ref [] in
-  let j = ref (length s) in
-  for i = length s - 1 downto 0 do
-    if unsafe_get s i = sep then begin
-      r := sub s (i + 1) (!j - i - 1) :: !r;
-      j := i
-    end
-  done;
-  sub s 0 !j :: !r
