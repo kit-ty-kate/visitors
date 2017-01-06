a62988a5ca390dd5f89b1a87a5936dbf0b0d5830
François Pottier <francois.pottier@inria.fr>
2017-01-06 19:06:19 +0100

    Switch from strings to atoms.

--- demos/lambda/lambda.ml
+++ demos/lambda/lambda.ml
@@ -1,33 +1,20 @@
-module StringSet = struct
-
-  include Set.Make(String)
-
-  let print xs =
-    iter (fun x ->
-      Printf.printf "%s\n" x
-    ) xs
-
-end
-
-module StringMap = Map.Make(String)
-
 type ('bn, 'term) abstraction =
   'bn * 'term
 
 module Nominal = struct
   module Abstraction = struct
     let iter _ f env (x, body) =
-      let env = StringSet.add x env in
+      let env = Atom.Set.add x env in
       f env body
   end
 end
 
 module Nominal2DeBruijn = struct
   type env =
-    int StringMap.t * int
+    int Atom.Map.t * int
   module Abstraction = struct
     let map _ f (env, n) (x, body) =
-      let env = StringMap.add x n env in
+      let env = Atom.Map.add x n env in
       (), f (env, n+1) body
   end
 end
@@ -35,10 +22,10 @@ end
 type void
 
 class fv = object
-  val mutable accu = StringSet.empty
+  val mutable accu = Atom.Set.empty
   method visit_'fn (env : 'env) x =
-    if not (StringSet.mem x env) then
-      accu <- StringSet.add x accu
+    if not (Atom.Set.mem x env) then
+      accu <- Atom.Set.add x accu
   method visit_'bn (_ : 'env) (_ : void) : unit =
     assert false (* never invoked *)
   method accu = accu
@@ -46,7 +33,7 @@ end
 
 class import = object
   method visit_'fn (env, n) x =
-    let level = StringMap.find x env in
+    let level = Atom.Map.find x env in
     n - level
   method visit_'bn (_ : Nominal2DeBruijn.env) (_ : void) : unit =
     assert false (* never invoked *)
@@ -64,17 +51,17 @@ type ('fn, 'bn) term =
 (* Nominal. *)
 
 type nominal_term =
-  (string, string) term
+  (Atom.t, Atom.t) term
 
 type db_term =
   (int, unit) term
 
-let fv (t : nominal_term) : StringSet.t =
+let fv (t : nominal_term) : Atom.Set.t =
   let o = object
     inherit [_] iter
     inherit fv
   end in
-  o # visit_term StringSet.empty t;
+  o # visit_term Atom.Set.empty t;
   o # accu
 
 let import (t : nominal_term) : db_term =
@@ -82,33 +69,35 @@ let import (t : nominal_term) : db_term =
     inherit [_] map
     inherit import
   end in
-  o # visit_term (StringMap.empty, 0) t
+  o # visit_term (Atom.Map.empty, 0) t
+
+let x = Atom.freshh "x"
 
 let identity =
-  TLambda ("x", TVar "x")
+  TLambda (x, TVar x)
 
 let y =
-  TVar "y"
+  TVar (Atom.freshh "y")
 
 let idy =
   TApp (identity, y)
 
 let () =
   print_endline "fv(\\x.x):";
-  StringSet.print (fv identity);
+  print_endline (Atom.Set.print (fv identity));
   print_endline "fv(y):";
-  StringSet.print (fv y);
+  print_endline (Atom.Set.print (fv y));
   print_endline "fv((\\x.x) y):";
-  StringSet.print (fv idy)
+  print_endline (Atom.Set.print (fv idy))
 
 (*
 
 class ['self] subst (sigma : name -> name) = object (self : 'self)
   (* TEMPORARY incorrect if [sigma x] is in [env]! need freshening *)
   method visit_name env x =
-    if StringSet.mem x env then x else sigma x
+    if Atom.Set.mem x env then x else sigma x
   method visit_binder term env (x, t) =
-    let env = StringSet.add x env in
+    let env = Atom.Set.add x env in
     x, term env t
 end
 
@@ -117,7 +106,7 @@ let subst (sigma : name -> name) (t : term) : term =
     inherit [_] map
     inherit [_] subst sigma
   end in
-  let env = StringSet.empty in
+  let env = Atom.Set.empty in
   subst#visit_term env t
 
 (* TEMPORARY need a term printer, too *)
