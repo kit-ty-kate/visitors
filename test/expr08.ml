a4bfd35a3c1fc791099d135ccb4f8b7e3782cd2e
François Pottier <francois.pottier@inria.fr>
2017-01-06 08:32:57 +0100

    Rename a data constructor.

--- test/expr08.ml
+++ test/expr08.ml
@@ -3,16 +3,16 @@ open Expr12 (* oexpr *)
 open Hashcons
 
 type expr =
-  E of expr oexpr hash_consed [@@unboxed]
+  H of expr oexpr hash_consed [@@unboxed]
 
 let table : expr oexpr Hashcons.t =
   create 128
 
 let make : expr oexpr -> expr =
-  fun e -> E (hashcons table e)
+  fun e -> H (hashcons table e)
 
 class ['self] map = object(self : 'self)
   inherit [_] omap
-  method visit_'expr env (E { node = e; _ }) =
+  method visit_'expr env (H { node = e; _ }) =
     make (self#visit_oexpr env e)
 end
