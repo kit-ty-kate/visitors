0b51ebb4e748aace094eb18da1566b7b2f501dc1
François Pottier <francois.pottier@inria.fr>
2017-01-11 00:00:08 +0100

    Quick test of [size].

--- demos/lambda/lambda.ml
+++ demos/lambda/lambda.ml
@@ -69,13 +69,20 @@ let y =
 let idy =
   TApp (identity, y)
 
+open Printf
+
 let () =
   print_endline "fa(\\x.x):";
   print_endline (Atom.Set.print (fa identity));
   print_endline "fa(y):";
   print_endline (Atom.Set.print (fa y));
   print_endline "fa((\\x.x) y):";
-  print_endline (Atom.Set.print (fa idy))
+  print_endline (Atom.Set.print (fa idy));
+  print_endline "size(\\x.x):";
+  printf  "%d\n" (size identity);
+  print_endline "size((\\x.x) y):";
+  printf  "%d\n" (size idy)
+
 
 (* TODO:
 
