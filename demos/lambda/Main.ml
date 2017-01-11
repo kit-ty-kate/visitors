16a76c1e7ef2a54d50e2f4293d0ca2e67d7792d6
François Pottier <francois.pottier@inria.fr>
2017-01-11 10:54:29 +0100

    More tests.

--- demos/lambda/Main.ml
+++ demos/lambda/Main.ml
@@ -2,32 +2,65 @@ open Printf
 open Term
 open Toolbox
 
-let x = Atom.freshh "x"
+(* Sample terms. *)
 
-let identity =
-  TLambda (x, TVar x)
+let x =
+  Atom.freshh "x"
 
 let y =
-  TVar (Atom.freshh "y")
+  Atom.freshh "y"
 
-let idy =
-  TApp (identity, y)
+let id =
+  TLambda (x, TVar x)
+
+let delta =
+  TLambda (x, TApp (TVar x, TVar x))
+
+let omega =
+  TApp (delta, copy delta)
+
+let samples = [
+    TVar y;
+    id;
+    TApp (id, TVar y);
+    TApp (id, copy id);
+    delta;
+    omega;
+  ]
+
+let evaluate f =
+  List.iter f samples
+
+(* A non-hygienic term printer. This printer shows the real (internal) identity
+   of atoms, using [Atom.show]. *)
+
+let nhprint oc t =
+  Print.term oc (show t)
 
 let print_fa t =
   (* This uses the debugging term printer, not the hygienic term printer. *)
   (* Similarly, it uses the debugging printer for sets of atoms. *)
   printf "fa(%a) = %a\n%!"
-    Print.term (show t)
+    nhprint t
     Atom.Set.print (fa t)
 
 let () =
-  List.iter print_fa [ y; identity; idy ]
+  evaluate print_fa
 
 let print_size t =
   (* This uses the debugging term printer, not the hygienic term printer. *)
   printf "size(%a) = %d\n%!"
-    Print.term (show t)
+    nhprint t
     (size t)
 
 let () =
-  List.iter print_size [ y; identity; idy ]
+  evaluate print_size
+
+let print_copy t =
+  (* This uses the debugging term printer, not the hygienic term printer. *)
+  printf "copy(%a) = %a\n%!"
+    nhprint t
+    nhprint (copy t)
+
+let () =
+  evaluate print_copy
