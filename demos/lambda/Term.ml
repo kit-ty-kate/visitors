ea90814ac56d5bb199a1e6e2c3d38eef0769c7e0
François Pottier <francois.pottier@inria.fr>
2017-01-11 13:37:26 +0100

    Cleanup in [Term].

--- demos/lambda/Term.ml
+++ demos/lambda/Term.ml
@@ -4,17 +4,49 @@ type ('fn, 'bn) term =
   | TVar of 'fn
   | TLambda of ('bn, ('fn, 'bn) term) abstraction
   | TApp of ('fn, 'bn) term * ('fn, 'bn) term
+
   [@@deriving
-    visitors { name = "Fa"; variety = "reduce"; monoid = "Atom.Set"; path = ["Fa"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "Show"; variety = "map"; path = ["Show"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "size"; variety = "reduce"; path = ["Size"]; freeze = ["bn"; "fn"]; monoid = "Monoid.Sum" },
-    visitors { name = "Atom2Unit"; variety = "iter"; path = ["Atom2Unit"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "Atom2DeBruijn"; variety = "map"; path = ["Atom2DeBruijn"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "String2Atom"; variety = "map"; path = ["String2Atom"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "Atom2String"; variety = "map"; path = ["Atom2String"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "Atom2Atom"; variety = "map"; path = ["Atom2Atom"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "Copy"; variety = "map"; path = ["Copy"]; freeze = ["bn"; "fn"]; final = true },
-    visitors { name = "atom2Something"; variety = "map"; path = ["Atom2Something"]; freeze = ["bn"; "fn"] }
+
+    visitors { name = "Fa";
+               variety = "reduce"; monoid = "Atom.Set"; path = ["Fa"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "Show";
+               variety = "map"; path = ["Show"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "size";
+               variety = "reduce"; monoid = "Monoid.Sum"; path = ["Size"];
+               freeze = ["bn"; "fn"]; final = false },
+
+    visitors { name = "Atom2Unit";
+               variety = "iter"; path = ["Atom2Unit"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "Atom2DeBruijn";
+               variety = "map"; path = ["Atom2DeBruijn"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "String2Atom";
+               variety = "map"; path = ["String2Atom"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "Atom2String";
+               variety = "map"; path = ["Atom2String"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "Atom2Atom";
+               variety = "map"; path = ["Atom2Atom"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "Copy";
+               variety = "map"; path = ["Copy"];
+               freeze = ["bn"; "fn"]; final = true },
+
+    visitors { name = "atom2Something";
+               variety = "map"; path = ["Atom2Something"];
+               freeze = ["bn"; "fn"]; final = false }
+
   ]
 
 type raw_term =
