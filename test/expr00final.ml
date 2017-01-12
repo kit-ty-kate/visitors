83a8f887b21b35f5fe67934893035fd812cd4c42
François Pottier <francois.pottier@inria.fr>
2017-01-12 09:43:55 +0100

    Remove two tests that involve [final].

--- test/expr00final.ml
+++
@@ -1,7 +0,0 @@
-type expr =
-  | EConst of int
-  | EAdd of expr * expr
-  [@@deriving visitors { name = "Iter"; variety = "iter"; final = true },
-              visitors { name = "Map"; variety = "map"; final = true },
-              visitors { name = "Iter2"; variety = "iter2"; final = true },
-              visitors { name = "Map2"; variety = "map2"; final = true }]
