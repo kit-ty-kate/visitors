c0dbbacce7497a2f772d2ab888ef9c59ed5baa0e
François Pottier <francois.pottier@inria.fr>
2017-01-18 21:34:39 +0100

    Add a test file that mentions all supported primitive types.

---
+++ test/testallprims.ml
@@ -0,0 +1,27 @@
+type t =
+  | Array of t array
+  | Bool of bool
+  | Bytes of bytes
+  | Char of char
+  | Float of float
+  | Int of int
+  | Int32 of int32
+  | Int64 of int64
+  | List of t list
+  | Nativeint of nativeint
+  | Option of t option
+  | Ref of t ref
+  | Result of (t, t) result
+  | String of string
+  | Unit of unit
+  | Tuple2 of (t * t)
+  | Tuple3 of (t * t * t)
+[@@deriving
+     visitors { variety = "iter" },
+     visitors { variety = "map" },
+     visitors { variety = "reduce" },
+     visitors { variety = "endo" },
+     visitors { variety = "iter2" },
+     visitors { variety = "map2" },
+     visitors { variety = "reduce2" }
+]
