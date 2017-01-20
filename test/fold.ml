7f76693900cb1205e7955c5891f9de6fa565ac90
François Pottier <francois.pottier@inria.fr>
2017-01-20 23:13:45 +0100

    Inline the class [convert].

--- test/fold.ml
+++ test/fold.ml
@@ -8,12 +8,11 @@ and crowd =
   | Someone of person * crowd
 [@@deriving visitors { variety = "fold" }]
 
-class convert = object
-  inherit [_] fold
-  method build_person  () f s = (f, s)
-  method build_Nobody  ()     = []
-  method build_Someone () p c = p :: c
-end
-
 let convert : crowd -> (string * string) list =
-  new convert # visit_crowd ()
+  let o = object
+    inherit [_] fold
+    method build_person  () f s = (f, s)
+    method build_Nobody  ()     = []
+    method build_Someone () p c = p :: c
+  end
+  in o # visit_crowd ()
