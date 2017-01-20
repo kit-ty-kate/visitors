97a2a3c82902cfb00399e817ed9d46a0c123c985
François Pottier <francois.pottier@inria.fr>
2017-01-20 21:14:04 +0100

    Remove useless method.

--- test/expr15b.ml
+++ test/expr15b.ml
@@ -4,7 +4,6 @@ let size : expr -> int =
   let o = object
     inherit [_] reduce as super
     inherit [_] VisitorsRuntime.addition_monoid
-    method! visit_EConst _ _ = 0
     method! visit_expr env e =
       1 + super # visit_expr env e
   end in
