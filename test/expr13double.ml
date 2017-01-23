11e372cf3f193a9484d453ab9e8669f08f0ba2b8
François Pottier <francois.pottier@inria.fr>
2017-01-23 09:43:26 +0100

    Define [double] more efficiently.

--- test/expr13double.ml
+++ test/expr13double.ml
@@ -1,10 +1,10 @@
 open Expr12
 open Expr13
 
-let double (e : expr) : expr =
+let double : expr -> expr =
   let v = object
     inherit [_] map
     method! visit_EConst _env k =
       EConst (2 * k)
   end in
-  v # visit_'expr () e
+  v # visit_'expr ()
