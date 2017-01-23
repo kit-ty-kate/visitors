82c357f1153d3ac95ba6542672b447318df2cbc6
François Pottier <francois.pottier@inria.fr>
2017-01-23 09:43:14 +0100

    Define [import] and [export] more efficiently.

--- test/expr14.ml
+++ test/expr14.ml
@@ -3,18 +3,18 @@ open Expr12 (* [oexpr] *)
 open Expr13 (*  [expr] *)
 open Expr08 (* [hexpr] *)
 
-let import (e : expr) : hexpr =
+let import : expr -> hexpr =
   let v = object (self)
     inherit [_] omap
     method visit_'expr _env (E e) =
       h (self#visit_oexpr _env e)
   end in
-  v # visit_'expr () e
+  v # visit_'expr ()
 
-let export (e : hexpr) : expr =
+let export : hexpr -> expr =
   let v = object (self)
     inherit [_] omap
     method visit_'expr _env (H { node = e; _ }) =
       E (self#visit_oexpr _env e)
   end in
-  v # visit_'expr () e
+  v # visit_'expr ()
