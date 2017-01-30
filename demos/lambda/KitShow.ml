eb87dd312a1dd7c8be829eccff89bcb84fe85d22
François Pottier <francois.pottier@inria.fr>
2017-01-30 21:05:09 +0100

    Removed the subdirectory demos/, moved to a separate repository.

--- demos/lambda/KitShow.ml
+++
@@ -1,8 +0,0 @@
-(* This kit serves to construct a [show] function for terms. *)
-
-(* At an abstraction or at a name occurrence, [Atom.show] is applied. *)
-
-class ['self] map = object (_ : 'self)
-  method private extend x env = Atom.show x, env
-  method private visit_'fn _env x = Atom.show x
-end
