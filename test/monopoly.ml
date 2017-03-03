096396a17a0b032d91a8199e4c9dd8eada4fe83a
François Pottier <francois.pottier@inria.fr>
2017-03-03 08:56:58 +0100

    New test file monopoly.ml.

---
+++ test/monopoly.ml
@@ -0,0 +1,12 @@
+type ('a, 'b) data =
+  | DataNil
+  | DataCons of 'a * 'b * ('a, 'b) data
+[@@deriving visitors { variety = "iter"; polymorphic = ["a"] },
+            visitors { variety = "map"; polymorphic = ["a"] },
+            visitors { variety = "endo"; polymorphic = ["a"] },
+            visitors { variety = "reduce"; polymorphic = ["a"] },
+            visitors { variety = "mapreduce"; polymorphic = ["a"] },
+            visitors { variety = "iter2"; polymorphic = ["a"] },
+            visitors { variety = "map2"; polymorphic = ["a"] },
+            visitors { variety = "reduce2"; polymorphic = ["a"] },
+            visitors { variety = "mapreduce2"; polymorphic = ["a"] }]
