ae4c6ce2a98e46eabbf74f79294999948beb2e30
François Pottier <francois.pottier@inria.fr>
2017-03-03 17:30:33 +0100

    Update the parameters in VisitorsRuntimeBootstrap.

--- test/VisitorsRuntimeBootstrap.ml
+++ test/VisitorsRuntimeBootstrap.ml
@@ -2,4 +2,4 @@ type 'a list =
   | []
   | (::) of 'a * 'a list
 
-[@@deriving visitors { variety = "iter"; data = false }]
+[@@deriving visitors { variety = "iter"; public = []; polymorphic = true; data = false; nude = true }]
