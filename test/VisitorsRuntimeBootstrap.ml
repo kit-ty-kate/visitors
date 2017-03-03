164f5d9bc612619d99c38da5182b492a87b32f05
François Pottier <francois.pottier@inria.fr>
2017-03-03 17:51:29 +0100

    Generate more code in [VisitorsRuntimeBootstrap].

--- test/VisitorsRuntimeBootstrap.ml
+++ test/VisitorsRuntimeBootstrap.ml
@@ -2,4 +2,25 @@ type 'a list =
   | []
   | (::) of 'a * 'a list
 
-[@@deriving visitors { variety = "iter"; public = []; polymorphic = true; data = false; nude = true }]
+and 'a option =
+  | None
+  | Some of 'a
+
+and 'a ref =
+  { mutable contents: 'a }
+
+and ('a, 'b) result =
+  | Ok of 'a
+  | Error of 'b
+
+[@@deriving
+visitors { variety = "iter"; public = []; polymorphic = true; data = false; nude = true },
+visitors { variety = "map"; public = []; polymorphic = true; data = false; nude = true },
+visitors { variety = "endo"; public = []; polymorphic = true; data = false; nude = true },
+visitors { variety = "reduce"; public = []; polymorphic = true; data = false; nude = true; ancestors = ["VisitorsRuntime.monoid"] },
+visitors { variety = "mapreduce"; public = []; polymorphic = true; data = false; nude = true; ancestors = ["VisitorsRuntime.monoid"] },
+visitors { variety = "iter2"; public = []; polymorphic = true; data = false; nude = true },
+visitors { variety = "map2"; public = []; polymorphic = true; data = false; nude = true },
+visitors { variety = "reduce2"; public = []; polymorphic = true; data = false; nude = true; ancestors = ["VisitorsRuntime.monoid"] },
+visitors { variety = "mapreduce2"; public = []; polymorphic = true; data = false; nude = true; ancestors = ["VisitorsRuntime.monoid"] }
+]
