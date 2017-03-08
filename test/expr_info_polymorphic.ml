098fb77df6a9bd26a8b9dcf97a1b18fbbb6b01cc
François Pottier <francois.pottier@inria.fr>
2017-03-08 09:46:58 +0100

    Split a test into two tests.

--- test/expr_info_polymorphic.ml
+++ test/expr_info_polymorphic.ml
@@ -1,16 +1,9 @@
 type 'info expr_node =
-  | EConst of (int[@opaque])
+  | EConst of int
   | EAdd of 'info expr * 'info expr
 
 and 'info expr =
   { info: 'info; node: 'info expr_node }
 
-[@@deriving visitors { variety = "iter"; polymorphic = true },
-            visitors { variety = "map"; polymorphic = true },
-            visitors { variety = "endo"; polymorphic = true },
-            visitors { variety = "reduce"; polymorphic = true },
-            visitors { variety = "mapreduce"; polymorphic = true },
-            visitors { variety = "iter2"; polymorphic = true },
-            visitors { variety = "map2"; polymorphic = true },
-            visitors { variety = "reduce2"; polymorphic = true },
-            visitors { variety = "mapreduce2"; polymorphic = true }]
+[@@deriving visitors
+  { variety = "map"; polymorphic = true; data = false }]
