4ae9d8b00235b82a305fcaa312453fb845c168b6
François Pottier <francois.pottier@inria.fr>
2017-03-08 10:30:26 +0100

    Spacing.

--- test/expr06.ml
+++ test/expr06.ml
@@ -11,5 +11,6 @@ and expr =
   | EConst of int
   | EUnOp of unop * expr
   | EBinOp of expr * binop * expr
-  [@@deriving visitors { variety = "iter" },
-              visitors { variety = "map"  }]
+
+[@@deriving visitors { variety = "iter" },
+            visitors { variety = "map"  }]
