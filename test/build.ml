5eb89f32699f6458e625fb11339febcc6182cc52
François Pottier <francois.pottier@inria.fr>
2017-03-15 16:28:57 +0100

    Updated test.

--- test/build.ml
+++ test/build.ml
@@ -3,5 +3,9 @@
 type foo =
   | A
   | B of int
-  | C of foo * foo
+  | C of foo * foo [@build fun x y -> C (x, y)]
+
+and point =
+  { x: int; y: int } [@@build fun x y -> { x; y }]
+
 [@@deriving visitors { variety = "map" }]
